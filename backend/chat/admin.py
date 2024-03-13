8c8278d2e2fc53643ec1c578195539a8c74d782c
nicholas-scharan <nicholas_scharan@mckinsey.com>
2024-03-12 21:36:10 -0300

    Creating Message Model

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("sender", "body")
+
 
 # Register your models here.
+
+admin.site.register(Message, MessageAdmin)
--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import time
 
 # Create your models here.
+
+
+class Message(models.Model):
+    session = models.CharField(max_length=120)
+    sender = models.CharField(max_length=120)
+    body = models.TextField()
+
+    def _str_(self):
+        return f"{self.sender} | {self.body}"
