66783d4efad47fc9147253722fef55ab20366b6a
nicholas-scharan <nicholas_scharan@mckinsey.com>
2024-03-12 23:23:13 -0300

    Adding React Frontend Starting App

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from .serializers import MessageSerializer
+from .models import Message
+
 # Create your views here.
+
+
+class MessageView(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    queryset = Message.objects.all()
