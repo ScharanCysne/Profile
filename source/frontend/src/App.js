5553443b87743cc202756e7037bcd87bc1500b39
nicholas-scharan <nicholas_scharan@mckinsey.com>
2024-03-20 20:43:08 -0300

    Added interactive cards

--- source/frontend/src/App.js
+++ source/frontend/src/App.js
@@ -1,17 +1,49 @@
 import './App.css'
 
-import { Container } from '@mui/material';
 import Divider from '@mui/material/Divider';
+import Curriculum from './component/curriculum';
 
+const SubHeader = () => {
+  return (
+    <div>
+      <h1
+        style={{
+          color: 'white',
+          fontFamily: 'avenir',
+          textAlign: 'center',
+        }}
+      >
+        Nicholas Scharan Cysne
+      </h1>
+      <Divider variant='middle' style={{color: 'white', border: '1px solid'}}/>
+      <h3
+        style={{
+          color: 'white',
+          fontFamily: 'avenir',
+          textAlign: 'center',
+          paddingLeft: '5em',
+          paddingRight: '5em',
+          marginBottom: '5em'
+        }}
+      >
+        <i>
+          "My goal is to develop the tools, systems, and processes that enable
+          artificial intelligence to be applied in the real world, thus
+          generating tangible and positive impact."
+        </i>
+      </h3>
+    </div>
+  )
+}
 
 
 function App() {
   return (
-    <Container className="App" >
+    <div className="App" style={{ marginLeft: '5%', marginRight: '5%', display: 'block'}}>
       <p style={{textAlign: 'center'}}><i class="arrow down"></i></p>
-      <Divider  style={{color: 'white', border: '1px solid'}}/>
-      <h1>Software Engineering | Artifical Intelligence | Data Science | MLOps </h1>
-    </Container>
+      <SubHeader/>
+      <Curriculum/>
+    </div>
   )
 }
 
--- source/frontend/src/component/curriculum.js
+++ source/frontend/src/component/curriculum.js
@@ -1,42 +1,42 @@
 import React, { Component } from "react";
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
-import './curriculum.css'; // Tell webpack that Button.js uses these styles
+import './curriculum.css';
 
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: 'lightgray',
-  ...theme.typography.body2,
+  backgroundColor: 'white',
   padding: theme.spacing(1),
   textAlign: 'left',
   color: theme.palette.text.secondary,
   marginBottom: '1em'
 }));
 
-class CurriculumItem extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {};
-    }
 
-    render() {
-        return (
-          <div>
-            <Item id="item_1">
-              <h3>Curriculum Item</h3>
-              <p>Standy by</p>
-            </Item>
-            <Item id="item_1">
-              <h3>Curriculum Item</h3>
-              <p>Standy by</p>
-            </Item>
-            <Item id="item_1">
-              <h3>Curriculum Item</h3>
-              <p>Standy by</p>
-            </Item>
-          </div>
-        );
-    }
+class Curriculum extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+      return (
+        <div>
+          <Item id="item_1">
+            <h3>Education</h3>
+            <p>Standy by</p>
+          </Item>
+          <Item id="item_1">
+            <h3>Experience</h3>
+            <p>Standy by</p>
+          </Item>
+          <Item id="item_1">
+            <h3>Personal Projects</h3>
+            <p>Standy by</p>
+          </Item>
+        </div>
+      );
+  }
 }
 
-export default CurriculumItem
+export default Curriculum
